6fb8bfaa773122b9fb200afac78707d9efd5896c
EL HOUCINE QARA <elhoucine.qara@harx.ai>
2026-01-25 05:26:58 +0100

    update

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance with the base URL from the environment variable
 const apiClient = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_API}/api`,
+  baseURL: `${import.meta.env.VITE_BACKEND_KNOWLEDGEBASE_API}/api`,
 });
 
 export default apiClient;
